5b79b22a137624738afed6f2bd3f3224a0b2c28d
mikan7296 <dumbmonkey10@gmail.com>
2022-11-25 08:22:38 +0800

    added posts.html/ changed scrollevent to jQuery

--- Scripts/scroll.js
+++ Scripts/scroll.js
@@ -1,11 +1,21 @@
-window.addEventListener('scroll', (event) => {
-    const scroll = window.scrollY;
-    const header = document.getElementById("header");
+// window.addEventListener('scroll', (event) => {
+//     const scroll = window.scrollY;
+//     const header = document.getElementsByClassName("canScroll");
 
-    if (scroll > 100) {
-        header.classList.add("active-header");
+//     if (scroll > 100) {
+//         header.classList.add("active-header");
+//     }
+//     else {
+//         header.classList.remove("active-header");
+//     }
+// })
+
+$(window).scroll(function() {
+    console.log($(window).scrollTop());
+    if ($(window).scrollTop() > 200) {
+        $('.canScroll').addClass('active-header');
     }
     else {
-        header.classList.remove("active-header");
+        $('.canScroll').removeClass('active-header');
     }
-})
+});
